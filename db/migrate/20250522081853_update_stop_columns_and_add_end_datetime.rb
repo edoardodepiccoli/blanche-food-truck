33d5b848f34364bbd378485c9a1bede0f485d16a
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-05-22 11:10:36 +0200

    set up rails app from scratch to start building the first MVP

--- db/migrate/20250522081853_update_stop_columns_and_add_end_datetime.rb
+++
@@ -1,6 +0,0 @@
-class UpdateStopColumnsAndAddEndDatetime < ActiveRecord::Migration[8.0]
-  def change
-    rename_column :stops, :datetime, :start_datetime
-    add_column :stops, :end_datetime, :datetime
-  end
-end
