c41e7334e28e379e989d814784fd11d41a367de4
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-05-22 11:23:18 +0200

    create Stop model and Stops controller

---
+++ db/migrate/20250522091406_create_stops.rb
@@ -0,0 +1,14 @@
+class CreateStops < ActiveRecord::Migration[8.0]
+  def change
+    create_table :stops do |t|
+      t.string :name
+      t.datetime :start_datetime
+      t.datetime :end_datetime
+      t.text :address
+      t.decimal :latitude
+      t.decimal :longitude
+
+      t.timestamps
+    end
+  end
+end
